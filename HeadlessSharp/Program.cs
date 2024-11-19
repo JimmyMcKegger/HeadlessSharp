742054dfa3ffe2f099816602a6be650dd9450c73
James <jamesrobertsongroves@gmail.com>
2024-11-19 02:03:37 +0000

    http-client

--- HeadlessSharp/Program.cs
+++ HeadlessSharp/Program.cs
@@ -1,6 +1,22 @@
 using HeadlessSharp.Components;
+using DotNetEnv;
+
+
+// get storefront API key from environment file
+Env.Load();
+string apiKey = Environment.GetEnvironmentVariable("STOREFRONT_API_TOKEN");
+string domain = Environment.GetEnvironmentVariable("STOREFRONT_DOMAIN");
 
 var builder = WebApplication.CreateBuilder(args);
+
+// add http client
+// https://learn.microsoft.com/en-us/aspnet/core/blazor/call-web-api?view=aspnetcore-8.0
+builder.Services.AddHttpClient("ShopifyClient", client =>
+{
+    client.BaseAddress = new Uri(domain);
+    client.DefaultRequestHeaders.Add("X-Shopify-Access-Token", apiKey);
+});
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
