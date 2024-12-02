34e31398e9de30d2fca99998e5e1ac903db3174c
James <jamesrobertsongroves@gmail.com>
2024-12-02 23:49:52 +0000

    add cart and caches

---
+++ HeadlessSharp/Cart.cs
@@ -0,0 +1,24 @@
+namespace HeadlessSharp;
+
+public class Cart
+{
+    // fields
+    private string id;
+    private double total;
+    private double discount;
+    private string checkoutUrl;
+    private string note;
+    
+    
+    // Getters adn setters
+    public string Id { get; set; }
+    public double Total { get; set; }
+    public double Discount { get; set; }
+    public string CheckoutUrl { get; set; }
+    public string Note { get; set; }
+    
+    // Constructor
+    
+    // Methods
+    // public Cart cartLinesAdd
+}
--- HeadlessSharp/obj/Debug/net8.0/HeadlessSharp.AssemblyInfo.cs
+++ HeadlessSharp/obj/Debug/net8.0/HeadlessSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeadlessSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+742054dfa3ffe2f099816602a6be650dd9450c73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48aee1ac1f715bfd6f1d72b3c46c92a2ff72637c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeadlessSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeadlessSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
