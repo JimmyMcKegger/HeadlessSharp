3f6fe9d5bb711fc9c60a2f14502c009069a6d467
James <jamesrobertsongroves@gmail.com>
2024-12-08 21:57:15 +0000

    product card sets cookies

--- HeadlessSharp/wwwroot/scripts.js
+++ HeadlessSharp/wwwroot/scripts.js
@@ -23,3 +23,50 @@ window.getCookie = cookieName => {
     }
     return "";
 }
+
+window.CreateCart = variant => {
+    const myHeaders = new Headers();
+    myHeaders.append("X-Shopify-Storefront-Access-Token", "e1936fde69a6692d38517d05aab6dd73");
+    myHeaders.append("Content-Type", "application/json");
+
+    const graphql = JSON.stringify({
+        query: `
+            mutation cartCreate($input: CartInput) {
+              cartCreate(input: $input) {
+                cart {
+                  id
+                }
+                userErrors {
+                  field
+                  message
+                }
+                warnings {
+                  message
+                  code
+                }
+              }
+            }
+        `,
+        variables: {"input":{"lines":[{"merchandiseId": variant,"quantity":1}]}}
+    })
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: graphql,
+        redirect: "follow"
+    };
+
+    return fetch("https://317b61-f3.myshopify.com/api/2024-10/graphql.json", requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(json => {
+            console.log(json);
+            const cartId = json['data']['cartCreate']['cart']['id'];
+            console.log(cartId);
+            setCookie("cart", cartId);
+        });
+};
