16189d056d22ce22090d71ba101c7433b1a26e1c
James <jamesrobertsongroves@gmail.com>
2024-12-08 22:47:59 +0000

    load cart data

--- HeadlessSharp/wwwroot/scripts.js
+++ HeadlessSharp/wwwroot/scripts.js
@@ -24,11 +24,14 @@ window.getCookie = cookieName => {
     return "";
 }
 
-window.CreateCart = variant => {
-    const myHeaders = new Headers();
-    myHeaders.append("X-Shopify-Storefront-Access-Token", "e1936fde69a6692d38517d05aab6dd73");
-    myHeaders.append("Content-Type", "application/json");
+window.myHeaders = () => {
+    const h = new Headers();
+    h.append("X-Shopify-Storefront-Access-Token", "e1936fde69a6692d38517d05aab6dd73");
+    h.append("Content-Type", "application/json");
+    return h;
+}
 
+window.CreateCart = variant => {
     const graphql = JSON.stringify({
         query: `
             mutation cartCreate($input: CartInput) {
@@ -51,7 +54,7 @@ window.CreateCart = variant => {
     })
     const requestOptions = {
         method: "POST",
-        headers: myHeaders,
+        headers: myHeaders(),
         body: graphql,
         redirect: "follow"
     };
@@ -68,5 +71,70 @@ window.CreateCart = variant => {
             const cartId = json['data']['cartCreate']['cart']['id'];
             console.log(cartId);
             setCookie("cart", cartId);
+        });
+};
+
+window.getCartData = cartId => {
+    console.log("getCartData firing with: ");
+    console.log(cartId);
+    const graphql = JSON.stringify({
+        query: `
+            query getCartData($id: ID!){
+              cart(id: $id){
+                id
+                checkoutUrl
+                lines(first:10){
+                  nodes{
+                    merchandise{
+                      __typename
+                      ...on ProductVariant{
+                        id
+                      }
+                    }
+                    quantity
+                  }
+                }
+                discountAllocations{
+                  __typename
+                  ... on CartAutomaticDiscountAllocation{
+                    title
+                  }
+                  discountedAmount{
+                    amount
+                    currencyCode
+                  }
+                }
+                cost{
+                  subtotalAmount{
+                    amount
+                    currencyCode
+                  }
+                  
+                  totalAmount{
+                    amount
+                    currencyCode
+                  }
+                }
+              }
+            }
+        `,
+        variables: { "id": cartId }
+    })
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders(),
+        body: graphql,
+        redirect: "follow"
+    };
+
+    return fetch("https://317b61-f3.myshopify.com/api/2024-10/graphql.json", requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(json => {
+            console.log(json);
         });
 };
