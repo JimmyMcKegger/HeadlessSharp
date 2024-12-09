1a266c75d714f8c4cbc409ca276501d4dd2ca05d
James <jamesrobertsongroves@gmail.com>
2024-12-09 01:46:29 +0000

    working

--- HeadlessSharp/SfapiSubject.cs
+++ HeadlessSharp/SfapiSubject.cs
@@ -10,18 +10,14 @@ public class SfapiSubject : ISfapiSubject
 {
     // fields
     private List<IObserver> observers = new List<IObserver>();
-    private List<IObserver> cartObservers = new List<IObserver>();
-    // private Dictionary<string, string> tokens = new Dictionary<string, string>;
     private readonly GraphQLHttpClient graphqlClient;
     private static SfapiSubject _instance;
+    // private Dictionary<string, List<IObserver>> cartObservers = new Dictionary<string, List<IObserver>>();
     
-
     // getters and setters
     private string ResponseBody { get; set; }
     public List<IObserver> Observers { get; set; }
-    public List<IObserver> CartObservers { get; set; }
-    // public Dictionary<string, string> Tokens { get; set; }
-
+    public Dictionary<string, List<IObserver>> CartObservers { get; set; }
 
     public static SfapiSubject GetInstance(string apiKey = "", string domain = "", string apiVersion = "")
     {
@@ -39,7 +35,6 @@ public class SfapiSubject : ISfapiSubject
         graphqlClient = new GraphQLHttpClient($"https://{domain}/api/{apiVersion}/graphql.json", new NewtonsoftJsonSerializer());
         graphqlClient.HttpClient.DefaultRequestHeaders.Add("X-Shopify-Storefront-Access-Token", apiKey);
         Observers = new List<IObserver>();
-        CartObservers = new List<IObserver>();
     }
 
     // Get data for shop data observers
@@ -58,105 +53,22 @@ public class SfapiSubject : ISfapiSubject
         }
     }
     
-    // Get data for Cart observers
-    public async Task GetCartInfo()
-    {
-        // TODO: check for a cart cookie
-        // var cartId = that cookie
-        try
-        {
-            
-            // var cartQuery = GraphQlQueries.GetCartData(cartId);
-            // var response = await graphqlClient.SendQueryAsync<dynamic>(cartQuery);
-            // JObject cartInfo = response.Data;
-            // NotifyCartObservers(cartInfo);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"Error Getting Cart Data: {e.Message}");
-        }
-    }
-    
-    public async Task CartCreate(string variantId)
-    {
-        // cartCreate
-        Console.WriteLine("CREATING CART");
-        try
-        {
-            var request = GraphQlMutations.CartCreate(variantId);
-            var response = await graphqlClient.SendQueryAsync<dynamic>(request);
-            JObject cartInfo = response.Data;
-            Console.WriteLine(cartInfo);
-            NotifyCartObservers(cartInfo);
-            // cartId = cartInfo["cartCreate"]?["cart"]?["id"]?.ToString();
-            
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"Error creating cart: {e.Message}");
-        }
-        
-        
-    }
-    
-    // public async Task AddToCart(string variantId, string cartIdOrToken)
-    // {
-    //     //
-    //     bool isCart = cartIdOrToken.Contains("gid://shopify/Cart/");
-    //     
-    //     if (isCart)
-    //     {
-    //         
-    //         // TODO: CartLinesAdd
-    //
-    //         
-    //     }
-    //     else
-    //     {
-    //         // cartCreate
-    //         Console.WriteLine("CREATING CART");
-    //         try
-    //         {
-    //             var request = GraphQlMutations.CartCreate(variantId);
-    //             var response = await graphqlClient.SendQueryAsync<dynamic>(request);
-    //             JObject cartInfo = response.Data;
-    //             Console.WriteLine(cartInfo);
-    //             NotifyCartObservers(cartInfo);
-    //             cartId = cartInfo["cartCreate"]?["cart"]?["id"]?.ToString();
-    //             
-    //             Tokens[cartIdOrToken] = cartInfo["cartCreate"]?["token"]?.ToString();
-    //             
-    //             // TODO: How to set a cookie from here?
-    //             // https://stackoverflow.com/questions/77751794/i-am-trying-to-create-cookie-while-login-with-a-user-in-blazor-web-server-app-bu
-    //             
-    //             
-    //         }
-    //         catch (Exception e)
-    //         {
-    //             Console.WriteLine($"Error creating cart: {e.Message}");
-    //         }
-    //     }
-    //     
-    // }
-    // register with the SfapiSubject using the following method
     public void RegisterObserver(IObserver observer)
     {
-        Console.WriteLine($"ADDING OBSERVER: {observer}");
-        if (!observers.Contains(observer))
+        try
         {
-            observers.Add(observer);
+            if (!observers.Contains(observer))
+            {
+                Console.WriteLine($"ADDING OBSERVER: {observer}");
+                observers.Add(observer);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error registering observer: {e.Message}");
         }
     }
     
-    public void RegisterCartObserver(IObserver observer)
-    {
-        Console.WriteLine($"ADDING CART OBSERVER: {observer}");
-        if (!CartObservers.Contains(observer))
-        {
-            CartObservers.Add(observer);
-        }
-    }
-
     // unregister from the storefront API subject using the following method
     public void UnregisterObserver(IObserver observer)
     {
@@ -170,30 +82,10 @@ public class SfapiSubject : ISfapiSubject
             Console.WriteLine($"ERROR IN UNREGISTER: {e}");
         }
     }
-    
-    public void UnregisterCartObserver(IObserver observer)
-    {
-        try
-        {
-            Console.WriteLine($"REMOVING CART OBSERVER: {observer}");
-            CartObservers.Remove(observer);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"ERROR IN UNREGISTER Carts: {e}");
-        }
-    }
 
     public void NotifyObservers(JObject data)
     {
         foreach (var observer in observers)
-        {
-            observer.Update(data);
-        }
-    }
-    public void NotifyCartObservers(JObject data)
-    {
-        foreach (var observer in CartObservers)
         {
             observer.Update(data);
         }
